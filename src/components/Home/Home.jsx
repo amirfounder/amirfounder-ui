eff54159f3aeea76ed0a839dbbf035e1cb2f8393
Amir Sharapov <amir@amirfounder.com>
2022-06-20 17:43:12 -0500

    styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,10 +9,10 @@ export const Home = () => {
         <div className={`${styles.headerContainer} ${styles.contained}`}>
           <div className={styles.headerColumn} style={{userSelect: 'none'}}>
             <div style={{fontSize: '20px'}}>
-              <span style={{color: '#38B6FF', fontWeight: '800'}} >&#60;</span>
-              <span style={{color: '#333333', fontWeight: '600'}}>Amir</span>
-              <span style={{color: '#5271FF', fontWeight: '800'}}>/</span>
-              <span style={{color: '#5E17EB', fontWeight: '800'}}>&#62;</span>
+              <span style={{color: '#38B6FF', fontWeight: '700'}} >&#60;</span>
+              <span style={{color: '#333333', fontWeight: '400'}}>Amir</span>
+              <span style={{color: '#5271FF', fontWeight: '700'}}>/</span>
+              <span style={{color: '#5E17EB', fontWeight: '700'}}>&#62;</span>
             </div>
           </div>
           <div className={styles.headerColumn}>
@@ -26,7 +26,7 @@ export const Home = () => {
         <div style={{padding: '100px 0px 0px 0px'}} className={`${styles.heroContainer} ${styles.contained}`}>
           <h1>Amir Sharapov - Backend Software Engineer</h1>
           <p>
-            My name is Amir Sharapov and I am a Software Engineer II with <strong>1+ years of industry experience developing full-stack and backend software</strong>.
+            My name is Amir Sharapov and I am a Software Engineer II with 1+ years of industry experience developing full-stack and backend software.
             My goal is to develop ML / NN embedded applications to solve global problems.
           </p>
         </div>
@@ -35,16 +35,16 @@ export const Home = () => {
         <div className={styles.contained}>
           <h2>For Recruiters / Hiring Managers...</h2>
           <ul>
-            <li><a target='_blank' href="/go/resume-final-pdf">Resume - PDF</a></li>
-            <li><a target='_blank' href="/go/resume-final-docx">Resume - DOCX</a></li>
-            <li><a target='_blank' href="/go/resume-source">Source</a></li>
+            <li><a className={styles.a} target='_blank' href="/go/resume-final-pdf">Resume - PDF</a></li>
+            <li><a className={styles.a}target='_blank' href="/go/resume-final-docx">Resume - DOCX</a></li>
+            <li><a className={styles.a}target='_blank' href="/go/resume-source">Source</a></li>
           </ul>
         </div>
         <div className={styles.containd}>
           <h2>Connect</h2>
           <ul>
-            <li><a target='_blank' href="/go/linkedin">LinkedIn</a></li>
-            <li><a target='_blank' href="/go/github">GitHub</a></li>
+            <li><a className={styles.a} target='_blank' href="/go/linkedin">LinkedIn</a></li>
+            <li><a className={styles.a} target='_blank' href="/go/github">GitHub</a></li>
           </ul>
         </div>
       </div>
